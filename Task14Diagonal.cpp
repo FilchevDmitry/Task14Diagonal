77c7f7df069bedfd01dd3cc9819722c892c198ba
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-04-24 15:50:06 +0300

    Добавьте файлы проекта.

---
+++ Task14Diagonal.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+void diagonal(int matrixx[][4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i != j)
+                matrixx[i][j] = 0;
+        }
+    }
+}
+void print(int matrixx[][4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << matrixx[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    cout << "Diagonal"<<endl;
+    int matrixx[4][4];
+    cout << "Enter the data" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> matrixx[i][j];
+        }
+    }
+    print(matrixx);
+    cout << endl;
+    diagonal(matrixx);
+    print(matrixx);
+}
+
